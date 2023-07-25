c2bc1e3e1097a9d4dccaf6aa0e4694399dcf9fd9
Christian Soltenborn <christian.soltenborn@fhdw.de>
2023-07-25 11:31:21 +0200

    as generated

---
+++ app/src/main/java/de/fhdw/app_entwicklung/chatgpt/MainActivity.java
@@ -0,0 +1,14 @@
+package de.fhdw.app_entwicklung.chatgpt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
