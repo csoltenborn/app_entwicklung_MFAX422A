ed0dd0562e197a4298b8b385b7dea8ba86d6613a
Christian Soltenborn <christian.soltenborn@fhdw.de>
2023-07-25 16:02:04 +0200

    ChatGpt is queried, and answer is displayed in TextView

--- app/src/main/java/de/fhdw/app_entwicklung/chatgpt/MainActivity.java
+++ app/src/main/java/de/fhdw/app_entwicklung/chatgpt/MainActivity.java
@@ -3,8 +3,16 @@ package de.fhdw.app_entwicklung.chatgpt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final ExecutorService backgroundExecutorService = Executors.newFixedThreadPool(4);
+    public static final Handler uiThreadHandler = new Handler(Looper.getMainLooper());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/de/fhdw/app_entwicklung/chatgpt/MainFragment.java
+++ app/src/main/java/de/fhdw/app_entwicklung/chatgpt/MainFragment.java
@@ -4,8 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import de.fhdw.app_entwicklung.chatgpt.openai.ChatGpt;
 
 public class MainFragment extends Fragment {
 
@@ -16,6 +22,28 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getButton().setOnClickListener(v -> MainActivity.backgroundExecutorService.execute(() ->
+        {
+            ChatGpt chatGpt = new ChatGpt("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");
+            String answer = chatGpt.getChatCompletion("What's the answer to the universe and everything?");
+            getTextView().setText(answer);
+        }));
+    }
+
+    private TextView getTextView() {
+        //noinspection ConstantConditions
+        return getView().findViewById(R.id.textView);
+    }
+
+    private Button getButton() {
+        //noinspection ConstantConditions
+        return getView().findViewById(R.id.button_ask);
     }
 
 }
