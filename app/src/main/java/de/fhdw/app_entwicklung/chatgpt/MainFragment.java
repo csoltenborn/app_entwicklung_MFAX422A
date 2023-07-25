5094c2ffb7dd3d51fccf33578febf9bebf734d39
Christian Soltenborn <christian.soltenborn@fhdw.de>
2023-07-25 11:54:09 +0200

    added MainFragment

---
+++ app/src/main/java/de/fhdw/app_entwicklung/chatgpt/MainFragment.java
@@ -0,0 +1,21 @@
+package de.fhdw.app_entwicklung.chatgpt;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+}
