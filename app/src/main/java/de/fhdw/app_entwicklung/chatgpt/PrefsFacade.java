866735dbe108bfbe427ff99441d3d6b5c091a25e
onix <onix@mt-ag.com>
2023-11-30 16:09:41 +0100

    Projekt upload

--- app/src/main/java/de/fhdw/app_entwicklung/chatgpt/PrefsFacade.java
+++ app/src/main/java/de/fhdw/app_entwicklung/chatgpt/PrefsFacade.java
@@ -5,9 +5,12 @@ import android.content.Context;
 import androidx.annotation.NonNull;
 import androidx.preference.PreferenceManager;
 
-public class PrefsFacade {
+import java.util.Locale;
+
+public class PrefsFacade  {
 
     private final Context context;
+
 
     public PrefsFacade(@NonNull Context context) {
         this.context = context;
@@ -15,6 +18,39 @@ public class PrefsFacade {
 
     public String getApiToken() {
         return PreferenceManager.getDefaultSharedPreferences(context).getString("api_token", "");
+    }
+
+    public Locale getLocale() {
+        String language = PreferenceManager.getDefaultSharedPreferences(context).getString("language", "de");
+        switch (language) {
+            case "de":
+                return Locale.GERMAN;
+            case "en":
+                return Locale.ENGLISH;
+            case "ita":
+                return Locale.ITALIAN;
+            case "franc":
+                return Locale.FRANCE;
+            default:
+                throw new RuntimeException("Local not supported: " + language);
+        }
+    }
+
+    public String getChatgptLanguage() {
+        String chatgpt_language = PreferenceManager.getDefaultSharedPreferences(context).getString
+                ("chatgpt_language", "English");
+        switch(chatgpt_language) {
+            case "German":
+                return "Übersetzt mir das auf deutsch:";
+            case "English":
+                return "Translate it in English:";
+            case "Italian":
+                return "Translate the following to italy:";
+            case "French":
+                return "Translate the following to france:";
+            default:
+                throw new RuntimeException("Output Language not supported: " + chatgpt_language);
+        }
     }
 
 }
